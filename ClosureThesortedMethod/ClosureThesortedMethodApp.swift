5d29b17f8183014ff043d633b068d083c1d405be
Wongi <hpurs95@gmail.com>
2022-07-01 01:58:48 +0900

    Initial Commit

---
+++ ClosureThesortedMethod/ClosureThesortedMethodApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosureThesortedMethodApp.swift
+//  ClosureThesortedMethod
+//
+//  Created by MacBook Air on 2022/07/01.
+//
+
+import SwiftUI
+
+@main
+struct ClosureThesortedMethodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
